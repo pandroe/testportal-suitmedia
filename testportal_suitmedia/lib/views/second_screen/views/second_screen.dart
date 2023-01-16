e159279413219cb40e6523369e5e2321bcd3f5a9
pandroe <daffapandu2304@gmail.com>
2023-01-16 23:14:21 +0700

    membuat ui design third screen

--- testportal_suitmedia/lib/views/second_screen/views/second_screen.dart
+++ testportal_suitmedia/lib/views/second_screen/views/second_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:testportal_suitmedia/utils/constants.dart';
 import 'package:testportal_suitmedia/views/second_screen/widgets/app_bar_widget.dart';
 import 'package:testportal_suitmedia/views/second_screen/widgets/choose_a_user_button.dart';
-
+import 'package:testportal_suitmedia/views/third_screen/views/third_screen.dart';
 
 class SecondScreen extends StatefulWidget {
   static const String routeName = 'second-screen';
@@ -23,21 +23,38 @@ class _SecondScreenState extends State<SecondScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-           Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text('Welcome', style: TextStyle(fontSize: Constants.fontSmall, fontWeight: FontWeight.w400),),
-              Text('John Doe', style: TextStyle(fontSize: Constants.fontSemiBig, fontWeight: FontWeight.bold),),
-            ],
-           ),
             Column(
-            children: [
-              Text('Selected User Name', style: TextStyle(fontSize: Constants.fontExtraBig, fontWeight: FontWeight.bold),),
-            ],
-           ),
-           ChooseAUserButton(onPressed: () {
-             
-           },)
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Welcome',
+                  style: TextStyle(
+                      fontSize: Constants.fontSmall,
+                      fontWeight: FontWeight.w400),
+                ),
+                Text(
+                  'John Doe',
+                  style: TextStyle(
+                      fontSize: Constants.fontSemiBig,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Text(
+                  'Selected User Name',
+                  style: TextStyle(
+                      fontSize: Constants.fontExtraBig,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+            ChooseAUserButton(
+              onPressed: () {
+                Navigator.pushNamed(context, ThirdScreen.routeName);
+              },
+            )
           ],
         ),
       ),
